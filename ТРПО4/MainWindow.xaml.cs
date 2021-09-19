00ffba9d8e730f35f4ce9d708630ece0ac3837da
Дарья Мондраева <186184@edu.fa.ru>
2021-09-19 11:20:54 +0300

    ПР 4

--- ТРПО4/MainWindow.xaml.cs
+++ ТРПО4/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+
 
 namespace ТРПО4
 {
@@ -65,6 +70,70 @@ namespace ТРПО4
             Application.Current.Resources.Clear();
          
             Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+
+        static void Export()
+        {
+            string path = "export.txt";
+            StreamWriter sw = new StreamWriter(path);
+
+            using (var db = new Entities())
+            {
+                var User = db.User.AsNoTracking().ToList();
+
+                string IDline = String.Join(":", db.User.Select(x => x.ID));
+                sw.WriteLine(IDline);
+                string Loginline = String.Join(":", db.User.Select(x => x.Login));
+                sw.WriteLine(Loginline);
+                string Passwordline = String.Join(":", db.User.Select(x => x.Password));
+                sw.WriteLine(Passwordline);
+                string FIOline = String.Join(":", db.User.Select(x => x.FIO));
+                sw.WriteLine(FIOline);
+                string Roleline = String.Join(":", db.User.Select(x => x.Role));
+                sw.WriteLine(Roleline);
+                sw.Close();
+                Process.Start("notepad.exe", path);
+            }
+        }
+
+        private void export_click(object sender, RoutedEventArgs e)
+        {
+            Export();
+        }
+
+        void Import()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            if (ofd.FileName != "")
+            {
+                StreamReader sr = new StreamReader(ofd.FileName);
+                while (!sr.EndOfStream)
+                {
+                    string[] lines = new string[5];
+                    for (int i = 0; i < 5; i++)
+                    {
+                        string line = sr.ReadLine();
+                        string[] data = line.Split(':');
+                        line = "";
+                        if (data.Length >= 2)
+                        {
+                            for (int j = 0; j < data.Length; j++)
+                            {
+                                line += data[j];
+                            }
+                        }
+                        lines[i] = line;
+                    }
+                    MessageBox.Show("Данные в файле: \nID: " + lines[0] + "\nЛогин: " + lines[1] + "\nПароль: " + lines[2] + "\nРоль: " + lines[3] + "\nФИО: " + lines[4]);
+                }
+            }
+            else MessageBox.Show("Файл не выбран");
+        }
+
+        private void import_click(object sender, RoutedEventArgs e)
+        {
+            Import();
         }
     }
 }
